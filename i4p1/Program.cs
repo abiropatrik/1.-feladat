8251e686210c623a7a3f4efa0d691fa6792adada
abiropatrik <103270571+abiropatrik@users.noreply.github.com>
2022-04-21 14:59:48 +0200

    A feladat b részében egy rejtjelezett üzenetet kell visszaalakítani kulcs megadásával,. Ehhez segítségül vesszük az előző függvényt.

--- i4p1/Program.cs
+++ i4p1/Program.cs
@@ -50,6 +50,51 @@ namespace i4p1
             string megoldas = new string(megoldaskarakterek);
             return megoldas;
         }
+
+        public static string Feladat1b(string kod, string kulcs)
+        {
+
+            char[] uzenetkarakterek = uzenet.ToCharArray();
+            int[] uzenetkod = new int[uzenetkarakterek.Length];
+
+
+            char[] kulcskarakterek = kulcs.ToCharArray();
+            int[] kulcskod = new int[kulcskarakterek.Length];
+
+            char a = 'a';
+
+            int[] megoldaskod = new int[uzenet.Length];
+            char[] megoldaskarakterek = new char[uzenet.Length];
+
+            for (int i = 0; i < uzenetkarakterek.Length; i++)
+            {
+                if (uzenetkarakterek[i].Equals(' '))
+                {
+                    uzenetkod[i] = 26;
+                }
+                else
+                {
+                    uzenetkod[i] = uzenetkarakterek[i] - a;
+                }
+                if (kulcskarakterek[i].Equals(' '))
+                {
+                    kulcskod[i] = 26;
+                }
+                else
+                {
+                    kulcskod[i] = kulcskarakterek[i] - a;
+                }
+                megoldaskod[i] = kulcskod[i] + uzenetkod[i];
+                if (kulcskod[i] + uzenetkod[i] > 26)
+                {
+                    megoldaskod[i] = (kulcskod[i] + uzenetkod[i]) % 27;
+                }
+                megoldaskarakterek[i] = Convert.ToChar(megoldaskod[i] + a);
+            }
+            string megoldas = new string(megoldaskarakterek);
+            return megoldas;
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Adja meg az üzenetet: ");
