8e8b26fbbe4a286501a7463c6cc58c6efab2094c
abiropatrik <103270571+abiropatrik@users.noreply.github.com>
2022-04-21 14:33:44 +0200

    Létrehozok int tömböket, amikbe a karakterek kódjai fognak kerülni.  A karakterek kódjait úgy kapom meg, ha minden egyes karakterből kivonom az a karaktert.

--- i4p1/Program.cs
+++ i4p1/Program.cs
@@ -11,9 +11,18 @@ namespace i4p1
         public static string Feladat1a(string uzenet, string kulcs)
         {
             char[] uzenetkarakterek = uzenet.ToCharArray();
+            int[] uzenetkod = new int[uzenetkarakterek.Length];
+
 
             char[] kulcskarakterek = kulcs.ToCharArray();
+            int[] kulcskod = new int[kulcskarakterek.Length];
 
+            char a = 'a';
+
+            for (int i = 0; i < uzenetkarakterek.Length; i++)
+            {
+                uzenetkod[i] = uzenetkarakterek[i] - a;
+            }
             return "0";
         }
         static void Main(string[] args)
